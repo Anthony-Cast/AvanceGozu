b5cdf661a49e822d9458eb168cfff94746253749
anthonycast <acastanedap@pucp.edu.pe>
2021-05-07 23:42:47 -0500

    detalles

--- src/main/java/com/example/avances/controller/AdminRestauranteController.java
+++ src/main/java/com/example/avances/controller/AdminRestauranteController.java
@@ -11,10 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
@@ -30,7 +27,7 @@ public class AdminRestauranteController {
     @Autowired
     RestauranteRepository restauranteRepository;
 
-    @GetMapping("/login")
+    @GetMapping("/loginadmin")
     public String loginAdmin(){
         return "AdminRestaurantes/login";
     }
