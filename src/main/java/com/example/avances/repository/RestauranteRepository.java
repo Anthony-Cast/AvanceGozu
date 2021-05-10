66884db309b93265462303f72a73a557a78765eb
joseluismunozzuta <joseluismunozzuta@gmail.com>
2021-05-10 13:45:47 -0500

    Avance

--- src/main/java/com/example/avances/repository/RestauranteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.avances.repository;
-
-import com.example.avances.entity.Restaurante;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante,Integer> {
-}
