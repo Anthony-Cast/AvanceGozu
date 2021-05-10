7b04a3850b0c2354f290ac6323e2a682e2460890
anthonycast <acastanedap@pucp.edu.pe>
2021-05-10 10:48:24 -0500

    Perfil

--- src/main/java/com/example/avances/repository/RestauranteRepository.java
+++ src/main/java/com/example/avances/repository/RestauranteRepository.java
@@ -2,8 +2,12 @@ package com.example.avances.repository;
 
 import com.example.avances.entity.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante,Integer> {
+    @Query(value="select*from restaurante\n" +
+            "where idadminrest=?1",nativeQuery = true)
+    Restaurante obtenerperfilRest(int id);
 }
